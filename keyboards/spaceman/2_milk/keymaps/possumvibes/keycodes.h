7db73103c5c47bc60e364f8ed20b5832ae20007d
littlemer-the-second <littlemer-the@pm.me>
2022-01-10 21:56:10 -0700

    fixed the linux KC_LOCKS key

--- keyboards/spaceman/2_milk/keymaps/possumvibes/keycodes.h
+++ keyboards/spaceman/2_milk/keymaps/possumvibes/keycodes.h
@@ -7,15 +7,17 @@ enum layers {
     _MORSE,
 };
 
-enum custom_keycodes {
-    KC_LOCKS = SAFE_RANGE
-};
+// enum custom_keycodes {
+//     KC_LOCKS = SAFE_RANGE
+// };
 
 #define TH(k)   LT(0, k)
 
 #define PGU_MOR    LT(_MORSE, KC_PGUP)
 #define PGD_LOW    LT(_LOWER, KC_PGDN)
 #define KC_MORSE   TH(KC_DOT)
+
+#define KC_LOCKS   TH(KC_F24)
 
 bool override_th_hold(uint16_t hold_keycode, keyrecord_t *record);
 
