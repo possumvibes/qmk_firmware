873bcb54c19467eebc29c590187e7226cb2e2d43
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-02-05 18:46:47 -0700

    added info.json for clogv1

--- keyboards/smoresboards/clog/config.h
+++ keyboards/smoresboards/clog/config.h
@@ -21,13 +21,6 @@
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xBEEF
-#define PRODUCT_ID      0x5051
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Smores
-#define PRODUCT         Clog
-
 /* key matrix size */
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 5
