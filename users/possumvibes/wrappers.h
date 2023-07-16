0d423d6256c34fc0079c43a95af3923d7807d6d5
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-07-15 21:51:37 -0600

    qwerty has normal thumb keys

--- users/possumvibes/wrappers.h
+++ users/possumvibes/wrappers.h
@@ -28,7 +28,7 @@
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, \
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  XXXXXXX, \
-                               THM_LH0, THM_LH1, KC_BSPC, THM_RH0
+                               THM_LH0, THM_LH1, THM_RH1, THM_RH0
 
 // all the cool kids reference combos from an alpha layout they don't remember anymore
 #define LAYER_COMBOREF \
