24a61113651a45ecd2450a07a09e53e80fc150cf
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-07-15 21:15:49 -0600

    reverted combo timer reduction, 36 is best for default

--- users/possumvibes/combos.c
+++ users/possumvibes/combos.c
@@ -8,20 +8,39 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo){
         case r_crx_hm:
         case r_hrz_hm_mid_ind:
         case r_crx_dn:
-            return COMBO_TERM - 7; // 14 ms
+            return COMBO_TERM - 22; // 14 ms
 
         // top row shortcuts
         case l_hrz_up_mid_ind:
         case r_hrz_up_mid_ind:
         case r_hrz_up_ind_lat:
         case dmenu:
+            return COMBO_TERM - 18; // 18 ms
 
         // home row/block shortcuts
         case cut:
         case copy:
         case paste:
         case enter:
-            return COMBO_TERM - 3; // 18 ms
+            return COMBO_TERM - 19; // 17 ms
+
+        // home-bottom verticals
+        case click_l:
+        case click_r:
+        case click_lctl:
+        case r_vert_hm_ind:
+        case r_vert_hm_mid:
+            return COMBO_TERM - 15; // 21 ms
+
+        // home-bottom adjacent diagonals
+        case l_diag_hm_pnk:
+        case r_diag_hm_pnk:
+            return COMBO_TERM - 15;
+
+        // thumbs
+        case lthumbs:
+        case rthumbs:
+            return COMBO_TERM - 10; // 26 ms
 
         default:
             return COMBO_TERM;
--- users/possumvibes/config.h
+++ users/possumvibes/config.h
@@ -9,7 +9,7 @@
 #define TAP_CODE_DELAY 15               // Introduces delay between code taps so no keys get eaten.
 
 // Combos!
-#define COMBO_TERM 21                   // Gotta combo fast.
+#define COMBO_TERM 36                   // Gotta combo fast.
 #define COMBO_ONLY_FROM_LAYER 1         // compare only with the COMBOREF layer.
 #define COMBO_TERM_PER_COMBO            // Adjust combo term for ease of combo.
 #define COMBO_STRICT_TIMER              // all keys must be hit within the term from the first keypress.
