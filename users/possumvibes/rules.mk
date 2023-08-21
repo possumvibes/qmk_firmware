995c1022e05b0fba0faa7e491fc87d4dc3c1d6b4
Possumvibes <90935434+possumvibes@users.noreply.github.com>
2023-08-20 19:43:21 -0600

    renamed process_records.c to possumvibes.c

--- users/possumvibes/rules.mk
+++ users/possumvibes/rules.mk
@@ -14,7 +14,7 @@ GRAVE_ESC_ENABLE       = no
 AUDIO_ENABLE 	       = no
 
 VPATH += keyboards/gboards/ # using gboards combo code: see https://combos.gboards.ca/
-INTROSPECTION_KEYMAP_C = process_records.c
+INTROSPECTION_KEYMAP_C = possumvibes.c
 
 # Add custom features
 SRC += features/mod_lock.c
